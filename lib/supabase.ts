e93a1c52c2084ad3137f3ff8c2fcf7f03e36bab9
rjr3-usury <r@usury.com>
2026-06-09 19:17:26 +0000

    Fixed supabase connection syntax

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.https://iwpijniytknpyzatblfk.supabase.co
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3cGlqbml5dGtucHl6YXRibGZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDg4NTAsImV4cCI6MjA5NjU4NDg1MH0.mvhSv6tExIb7dpXU_YGHY2i-otUBv0B1dp-DsK3yrNc
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
